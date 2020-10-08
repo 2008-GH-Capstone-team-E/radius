79daccb477ea11c8959cb3ae57049d9e8d4a5faf
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-10-08 15:39:17 -0500

    testing(App.js): display map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './css/style.css';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
 
+
+const libraries = ["places"];
+
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh"
+}
+
+const center = {
+  lat: 40.712776,
+  lng: -74.005974
+}
 function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if(loadError) return "Error loading maps";
+  if(!isLoaded) return "Loading Maps";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Our New Project
-        </p>
-      </header>
+    <div>
+     <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center}></GoogleMap>
     </div>
   );
 }
