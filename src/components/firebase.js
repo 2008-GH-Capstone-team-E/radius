f23304bb30d730ad57d96ff12c0b170b3e556c9f
imedvesan <60087615+imedvesan@users.noreply.github.com>
2020-10-14 17:40:17 -0500

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyCFNovyH3WQ4iGx31u_ePucMj7Y6K4ytVI" ,
+  apiKey: "" ,
   authDomain: "radius-db5d1.firebaseapp.com",
   databaseURL: "https://radius-db5d1.firebaseio.com",
   projectId: "radius-db5d1",
   storageBucket: "radius-db5d1.appspot.com",
   messagingSenderId: "1088685100871",
-  appId: "1:1088685100871:web:c3e3ccf6e542df0df70662",
+  appId: "",
   measurementId: "G-58C436RVDN"
 };
 // Initialize Firebase
